c18313dc55af6e6de5f01a4dba4de2aad81fdfe1
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2022-10-17 13:59:08 +0200

    Fix workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 echo "Hello $1"
 pwd
 ls -al
-ls -al ..
 time=$(date)
 find src/
 
 #ls -al >> $GITHUB_ENV
 
 node /avalia/src/index.js
+
+ls -al
 find output
 mkdir -p $GITHUB_WORKSPACE/metrics
 # cp /avalia/src/output/* $GITHUB_WORKSPACE/metrics/
